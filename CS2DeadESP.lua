63434179b6376fd8e631427fbd71c59d0b24971b
KriZz87 <32612882+ticzz@users.noreply.github.com>
2023-12-09 23:22:23 +0100

    Update CS2DeadESP.lua

--- CS2DeadESP.lua
+++ CS2DeadESP.lua
@@ -18,12 +18,14 @@ local chamsOptions = gui.Combobox(espGroup, "chamsOptions", "Type of Chams", "Of
 local indicators_clr = gui.ColorPicker(espGroup, "indicator.color", "WH Indicator Color", 0, 0, 0, 255)
 local xposi = gui.Slider(espGroup, "deadesp_xposi", "X Position", 15, 0, x)
 local yposi = gui.Slider(espGroup, "deadesp_yposi", "Y Position", y / 2, 0, y)
+
+-- Option Tips
 gui.Text(espGroup, "Created by ticzz | aka KriZz87")
 gui.Text(espGroup, "https://github.com/ticzz/Aimware-v5-CS2-luas")
 holdKey:SetDescription("Key to turn on Chams thru Wallz while alive")
 chamsOptions:SetDescription("Chams used for onKey Wallhack and while dead")
-xposi:SetDescription("Sets X Screenposition for the Indicator")
-yposi:SetDescription("Sets Y Screenposition for the Indicator")
+xposi:SetDescription("Sets position X  for the Indicator")
+yposi:SetDescription("Sets position Y for the Indicator")
 local color = draw.Color
 local text = draw.TextShadow
 local font = draw.CreateFont("Arial", 12, 100)
@@ -34,104 +36,104 @@ local toggle = false
 
 -- Enable extra ESP visuals when dead
 local function EnableExtraESP()
-	-- Force enable enemy chams behind walls
-	gui.SetValue("esp.chams.enemy.occluded", 1)
-	-- Enable other extras
-	gui.SetValue("esp.chams.enemy.visible", 0)
-	gui.SetValue("esp.chams.enemyattachments.occluded", 0)
-	gui.SetValue("esp.chams.enemyattachments.visible", 0)
-	gui.SetValue("esp.chams.friendlyattachments.occluded", 0)
-	gui.SetValue("esp.chams.friendlyattachments.visible", 0)
-	gui.SetValue("esp.overlay.enemy.box", false)
-	gui.SetValue("esp.overlay.enemy.flags.hasdefuser", true)
-	gui.SetValue("esp.overlay.enemy.flags.hasc4", true)
-	gui.SetValue("esp.overlay.enemy.flags.reloading", false)
-	gui.SetValue("esp.overlay.enemy.flags.scoped", false)
-	gui.SetValue("esp.overlay.enemy.health.healthnum", true)
-	gui.SetValue("esp.overlay.enemy.health.healthbar", true)
-	gui.SetValue("esp.overlay.enemy.name", true)
-	gui.SetValue("esp.overlay.enemy.weapon", true)
-	gui.SetValue("esp.overlay.weapon.ammo", true)
-	gui.SetValue("esp.overlay.enemy.barrel", false)
-	gui.SetValue("esp.overlay.enemy.armor", true)
+ -- Force enable enemy chams behind walls
+ gui.SetValue("esp.chams.enemy.occluded", 1)
+ -- Enable other extras
+ gui.SetValue("esp.chams.enemy.visible", 0)
+ gui.SetValue("esp.chams.enemyattachments.occluded", 0)
+ gui.SetValue("esp.chams.enemyattachments.visible", 0)
+ gui.SetValue("esp.chams.friendlyattachments.occluded", 0)
+ gui.SetValue("esp.chams.friendlyattachments.visible", 0)
+ gui.SetValue("esp.overlay.enemy.box", false)
+ gui.SetValue("esp.overlay.enemy.flags.hasdefuser", true)
+ gui.SetValue("esp.overlay.enemy.flags.hasc4", true)
+ gui.SetValue("esp.overlay.enemy.flags.reloading", false)
+ gui.SetValue("esp.overlay.enemy.flags.scoped", false)
+ gui.SetValue("esp.overlay.enemy.health.healthnum", true)
+ gui.SetValue("esp.overlay.enemy.health.healthbar", true)
+ gui.SetValue("esp.overlay.enemy.name", true)
+ gui.SetValue("esp.overlay.enemy.weapon", true)
+ gui.SetValue("esp.overlay.weapon.ammo", true)
+ gui.SetValue("esp.overlay.enemy.barrel", false)
+ gui.SetValue("esp.overlay.enemy.armor", true)
 end
 
 -- Disable most extras when alive
 local function DisableExtraESP()
-	-- Restore visible chams only
-	gui.SetValue("esp.chams.enemy.visible", chamsOptions:GetValue())
-	-- Disable other extras
-	gui.SetValue("esp.chams.enemy.occluded", 0)
-	gui.SetValue("esp.chams.enemyattachments.occluded", 0)
-	gui.SetValue("esp.chams.enemyattachments.visible", 0)
-	gui.SetValue("esp.chams.friendlyattachments.occluded", 0)
-	gui.SetValue("esp.chams.friendlyattachments.visible", 0)
-	gui.SetValue("esp.overlay.enemy.box", false)
-	gui.SetValue("esp.overlay.enemy.flags.hasdefuser", false)
-	gui.SetValue("esp.overlay.enemy.flags.hasc4", false)
-	gui.SetValue("esp.overlay.enemy.flags.reloading", false)
-	gui.SetValue("esp.overlay.enemy.flags.scoped", false)
-	gui.SetValue("esp.overlay.enemy.health.healthnum", false)
-	gui.SetValue("esp.overlay.enemy.health.healthbar", false)
-	gui.SetValue("esp.overlay.enemy.name", false)
-	gui.SetValue("esp.overlay.enemy.weapon", false)
-	gui.SetValue("esp.overlay.weapon.ammo", false)
-	gui.SetValue("esp.overlay.enemy.barrel", false)
-	gui.SetValue("esp.overlay.enemy.armor", false)
+ -- Restore visible chams only
+ gui.SetValue("esp.chams.enemy.visible", chamsOptions:GetValue())
+ -- Disable other extras
+ gui.SetValue("esp.chams.enemy.occluded", 0)
+ gui.SetValue("esp.chams.enemyattachments.occluded", 0)
+ gui.SetValue("esp.chams.enemyattachments.visible", 0)
+ gui.SetValue("esp.chams.friendlyattachments.occluded", 0)
+ gui.SetValue("esp.chams.friendlyattachments.visible", 0)
+ gui.SetValue("esp.overlay.enemy.box", false)
+ gui.SetValue("esp.overlay.enemy.flags.hasdefuser", false)
+ gui.SetValue("esp.overlay.enemy.flags.hasc4", false)
+ gui.SetValue("esp.overlay.enemy.flags.reloading", false)
+ gui.SetValue("esp.overlay.enemy.flags.scoped", false)
+ gui.SetValue("esp.overlay.enemy.health.healthnum", false)
+ gui.SetValue("esp.overlay.enemy.health.healthbar", false)
+ gui.SetValue("esp.overlay.enemy.name", false)
+ gui.SetValue("esp.overlay.enemy.weapon", false)
+ gui.SetValue("esp.overlay.weapon.ammo", false)
+ gui.SetValue("esp.overlay.enemy.barrel", false)
+ gui.SetValue("esp.overlay.enemy.armor", false)
 end
 
 -- Enable OnHold extra ESP visuals by onHold-/Togglekey
 local function OnHoldExtraESP()
-	gui.SetValue("esp.chams.enemy.occluded", 1)
+ gui.SetValue("esp.chams.enemy.occluded", 1)
 end
 
 -- Main logic
 local function UpdateESPForState()
-	draw.SetFont(font)
+ draw.SetFont(font)
 
-	-- Exit if not enabled
-	local lp = entities.GetLocalPlayer()
-	if not lp or not enableMaster:GetValue() then
-		return
-	end
+ -- Exit if not enabled
+ local lp = entities.GetLocalPlayer()
+ if not lp or not enableMaster:GetValue() then
+ return
+ end
 
-	-- Set ESP based on alive/dead state
-	if isAlive then
-		-- Alive logic - reduce ESP
-		DisableExtraESP()
+ -- Set ESP based on alive/dead state
+ if isAlive then
+ -- Alive logic - reduce ESP
+ DisableExtraESP()
 
-		-- Allow brief ESP on hold key
-		if holdKey:GetValue() ~= 0 and keyMode:GetValue() == 0 then
-			if input.IsButtonDown(holdKey:GetValue()) then
-				OnHoldExtraESP()
-				color(indicators_clr:GetValue())
-				text(xposi:GetValue(), yposi:GetValue(), "OnHold Chams")
-			end
-		elseif holdKey:GetValue() ~= 0 and keyMode:GetValue() == 1 then
-			if input.IsButtonPressed(holdKey:GetValue()) then
-				toggle = not toggle
-			end
-			if toggle then
-				OnHoldExtraESP()
-				color(indicators_clr:GetValue())
-				text(xposi:GetValue(), yposi:GetValue(), "WH Chams On")
-			else
-				DisableExtraESP()
-			end
-		end
-	else
-		-- Dead logic - enable extras
-		EnableExtraESP()
-	end
+ -- Allow brief ESP on hold key
+ if holdKey:GetValue() ~= 0 and keyMode:GetValue() == 0 then
+ if input.IsButtonDown(holdKey:GetValue()) then
+ OnHoldExtraESP()
+ color(indicators_clr:GetValue())
+ text(xposi:GetValue(), yposi:GetValue(), "OnHold Chams")
+ end
+ elseif holdKey:GetValue() ~= 0 and keyMode:GetValue() == 1 then
+ if input.IsButtonPressed(holdKey:GetValue()) then
+ toggle = not toggle
+ end
+ if toggle then
+ OnHoldExtraESP()
+ color(indicators_clr:GetValue())
+ text(xposi:GetValue(), yposi:GetValue(), "WH Chams On")
+ else
+ DisableExtraESP()
+ end
+ end
+ else
+ -- Dead logic - enable extras
+ EnableExtraESP()
+ end
 end
 
 -- Main loop
 local function OnDraw()
-	-- Get current player state
-	isAlive = entities.GetLocalPlayer():IsAlive()
+ -- Get current player state
+ isAlive = entities.GetLocalPlayer():IsAlive()
 
-	-- Update ESP based on state
-	UpdateESPForState()
+ -- Update ESP based on state
+ UpdateESPForState()
 end
 
 -- Register callbacks
