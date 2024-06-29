4d822e0dde619e48e464bd6145c0f9a9719a9773
KriZz87 <32612882+ticzz@users.noreply.github.com>
2024-06-29 16:08:00 +0200

    Update CS2DeadESP.lua

--- CS2DeadESP.lua
+++ CS2DeadESP.lua
@@ -1,44 +1,42 @@
---Inspired by zack´s [https://aimware.net/forum/user-36169.html] "always esp on dead.lua"
--- Main usage is for Legit Gameplay
--- Enable extra ESP visuals for dead players to help teammates
--- Disable most extras for alive players to hide cheats
--- Allow briefly enabling extras while alive via a toggle hotkey
+--[[
+DeadESP Script for Aimware
+Author: ticzz | aka KriZz87
+GitHub: https://github.com/ticzz/Aimware-v5-CS2-luas
 
--- Menu creation
-local x, y = draw.GetScreenSize()
-local vis_ref = gui.Reference("VISUALS")
-local esp_tab = gui.Tab(vis_ref, "deadesp.tab", "DeadESP")
-local espGroup = gui.Groupbox(esp_tab, "DeadESP", 10, 10, 300, 0)
+Description:
+This script enables extra ESP visuals for dead players to help teammates,
+while disabling most extras for alive players to hide cheats.
+It also allows briefly enabling extras while alive via a toggle hotkey.
 
--- Configurable options
-local enableMaster = gui.Checkbox(espGroup, "enablemaster", "Enable DeadESP", false)
-local keyMode = gui.Combobox(espGroup, "espkeymode", "keyMode", "Hold", "Toggle")
-local holdKey = gui.Keybox(espGroup, "espkey", "ESP Key", 0)
-local chamsOptions = gui.Combobox(espGroup, "chamsOptions", "Type of Chams", "Off", "Flat")
-local indicators_clr = gui.ColorPicker(espGroup, "indicator.color", "WH Indicator Color", 0, 0, 0, 255)
-local xposi = gui.Slider(espGroup, "deadesp_xposi", "X Position", 15, 0, x)
-local yposi = gui.Slider(espGroup, "deadesp_yposi", "Y Position", y / 2, 0, y)
+Inspired by zack's [https://aimware.net/forum/user-36169.html] "always esp on dead.lua"
+]]
 
--- Option Tips
-gui.Text(espGroup, "Created by ticzz | aka KriZz87")
-gui.Text(espGroup, "https://github.com/ticzz/Aimware-v5-CS2-luas")
-holdKey:SetDescription("Key to turn on Chams thru Wallz while alive")
-chamsOptions:SetDescription("Chams used for onKey Wallhack and while dead")
-xposi:SetDescription("Sets position X  for the Indicator")
-yposi:SetDescription("Sets position Y for the Indicator")
-local color = draw.Color
-local text = draw.TextShadow
-local font = draw.CreateFont("Verdana", 13, 800)
+local esp_tab = gui.Tab(gui.Reference("VISUALS"), "deadesp.tab", "DeadESP")
+local gui_group = gui.Groupbox(esp_tab, "DeadESP", 10, 10, 300, 0)
 
--- Internal state
-local isAlive = false
-local toggle = false
+local enabled = gui.Checkbox(gui_group, "enable", "Enable DeadESP", false)
+local key_mode = gui.Combobox(gui_group, "keymode", "Key Mode", "Hold", "Toggle")
+local toggle_key = gui.Keybox(gui_group, "togglekey", "Toggle Key", 0)
+local chams_type = gui.Combobox(gui_group, "chamsTypes", "Chams Type", "Off", "Flat")
+local indicator_color = gui.ColorPicker(gui_group, "indicator.color", "Indicator Color", 0, 0, 0, 255)
+local indicator_x = gui.Slider(gui_group, "Indicator X Position", 15, 0, draw.GetScreenSize())
+local indicator_y = gui.Slider(gui_group, "Indicator Y Position", draw.GetScreenSize() / 2, 0, draw.GetScreenSize())
 
--- Enable extra ESP visuals when dead
+gui.Text(gui_group, "Created by ticzz | aka KriZz87")
+gui.Text(gui_group, "https://github.com/ticzz/Aimware-v5-CS2-luas")
+key_mode:SetDescription(key_mode, "Switch between onhold key and perm toggle key modes")
+toggle_key:SetDescription(toggle_key, "Key to turn on Chams through walls while alive")
+chams_type:SetDescription(chams_type, "Chams used for on-key wallhack and while dead")
+indicator_color:SetDescription(indicator_color, "Sets the color of the indicator")
+indicator_x:SetDescription(indicator_x, "Sets the X position for the indicator")
+indicator_y:SetDescription(indicator_y, "Sets the Y position for the indicator")
+
+local font = draw.AddFont("Verdana", 13, 800)
+local is_alive = false
+local is_toggled = false
+
 local function EnableExtraESP()
-	-- Force enable enemy chams behind walls
 	gui.SetValue("esp.chams.enemy.occluded", 1)
-	-- Enable other extras
 	gui.SetValue("esp.chams.enemy.visible", 1)
 	gui.SetValue("esp.chams.enemyattachments.occluded", 0)
 	gui.SetValue("esp.chams.enemyattachments.visible", 0)
@@ -57,11 +55,8 @@ local function EnableExtraESP()
 	gui.SetValue("esp.overlay.enemy.armor", true)
 end
 
--- Disable most extras when alive
 local function DisableExtraESP()
-	-- Restore visible chams only
-	gui.SetValue("esp.chams.enemy.visible", chamsOptions:GetValue())
-	-- Disable other extras
+	gui.SetValue("esp.chams.enemy.visible", chams_type:GetValue())
 	gui.SetValue("esp.chams.enemy.occluded", 0)
 	gui.SetValue("esp.chams.enemyattachments.occluded", 0)
 	gui.SetValue("esp.chams.enemyattachments.visible", 0)
@@ -80,70 +75,58 @@ local function DisableExtraESP()
 	gui.SetValue("esp.overlay.enemy.armor", false)
 end
 
--- Enable OnHold extra ESP visuals by onHold-/Togglekey
-local function OnHoldExtraESP()
+local function EnableOnHoldESP()
 	gui.SetValue("esp.chams.enemy.occluded", 1)
 end
 
--- Main logic
-local function UpdateESPForState()
+local function UpdateESP()
 	draw.SetFont(font)
 
-	-- Exit if not enabled
-	local lp = entities.GetLocalPlayer()
-	if not lp or not enableMaster:GetValue() then
+	local local_player = entities.GetLocalPlayer()
+	if not local_player or not enabled:GetValue() then
 		return
 	end
 
-	-- Set ESP based on alive/dead state
-	if isAlive then
-		-- Alive logic - reduce ESP
+	if not toggle_key:GetValue() then
+		return
+	end
+
+	is_alive = local_player:IsAlive()
+
+	if is_alive then
 		DisableExtraESP()
 
-		-- Allow brief ESP on hold key
-		if holdKey:GetValue() ~= 0 and keyMode:GetValue() == 0 then
-			if input.IsButtonDown(holdKey:GetValue()) then
-				OnHoldExtraESP()
-				color(indicators_clr:GetValue())
-				text(xposi:GetValue(), yposi:GetValue(), "OnHold Chams")
+		if key_mode:GetValue() == 0 then -- Hold mode
+			if input.IsButtonDown(toggle_key:GetValue()) then
+				EnableOnHoldESP()
+				draw.Color(indicator_color:GetValue())
+				draw.TextShadow(indicator_x:GetValue(), indicator_y:GetValue(), "OnHold Chams")
 			else
 				DisableExtraESP()
 			end
-		elseif holdKey:GetValue() ~= 0 and keyMode:GetValue() == 1 then
-			if input.IsButtonPressed(holdKey:GetValue()) then
-				if not toggle then
-					toggle = true
-				else
-					toggle = false
-				end
-			elseif toggle then
-				OnHoldExtraESP()
-				color(indicators_clr:GetValue())
-				text(xposi:GetValue(), yposi:GetValue(), "WH Chams On")
+		else -- Toggle mode
+			if input.IsButtonPressed(toggle_key:GetValue()) then
+				is_toggled = not is_toggled
+			end
+
+			if is_toggled then
+				EnableOnHoldESP()
+				draw.Color(indicator_color:GetValue())
+				draw.TextShadow(indicator_x:GetValue(), indicator_y:GetValue(), "WH Chams On")
 			else
 				DisableExtraESP()
 			end
 		end
 	else
-		-- Dead logic - enable extras
 		EnableExtraESP()
 	end
 end
 
--- Main loop
-local function OnDraw()
-	if not entities.GetLocalPlayer() then
-		return
-	end
-	-- Get current player state
-	isAlive = entities.GetLocalPlayer():IsAlive()
+callbacks.Register("Draw", UpdateESP)
 
-	-- Update ESP based on state
-	UpdateESPForState()
-end
 
--- Register callbacks
-callbacks.Register("Draw", OnDraw)
 
 --***********************************************--
-print("♥♥♥ " .. GetScriptName() .. " loaded without Errors ♥♥♥")
+
+local gui_name = gui.GetScriptName()
+print("♥♥♥ " .. gui_name .. " loaded without errors ♥♥♥")
