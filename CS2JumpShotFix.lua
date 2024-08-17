5b8d1edb2298d766caf28f60a2420086777d3e8a
KriZz87 <32612882+ticzz@users.noreply.github.com>
2024-08-17 15:20:20 +0200

    Update CS2JumpShotFix.lua

--- CS2JumpShotFix.lua
+++ CS2JumpShotFix.lua
@@ -5,13 +5,16 @@ local function get_weapon_class(weapon_id)
 		return "hpistol"
 	elseif weapon_id == 14 or weapon_id == 28 then
 		return "lmg"
-	elseif weapon_id == 2 or weapon_id == 3 or weapon_id == 4 or weapon_id == 30 or weapon_id == 32 or weapon_id == 36 or weapon_id == 61 or weapon_id == 63 then
+	elseif weapon_id == 2 or weapon_id == 3 or weapon_id == 4 or weapon_id == 30 or weapon_id == 32 or weapon_id == 36 or
+		weapon_id == 61 or weapon_id == 63 then
 		return "pistol"
-	elseif weapon_id == 7 or weapon_id == 8 or weapon_id == 10 or weapon_id == 13 or weapon_id == 16 or weapon_id == 39 or weapon_id == 60 then
+	elseif weapon_id == 7 or weapon_id == 8 or weapon_id == 10 or weapon_id == 13 or weapon_id == 16 or weapon_id == 39 or
+		weapon_id == 60 then
 		return "rifle"
 	elseif weapon_id == 40 then
 		return "scout"
-	elseif weapon_id == 17 or weapon_id == 19 or weapon_id == 23 or weapon_id == 24 or weapon_id == 26 or weapon_id == 33 or weapon_id == 34 then
+	elseif weapon_id == 17 or weapon_id == 19 or weapon_id == 23 or weapon_id == 24 or weapon_id == 26 or weapon_id == 33 or
+		weapon_id == 34 then
 		return "smg"
 	elseif weapon_id == 25 or weapon_id == 27 or weapon_id == 29 or weapon_id == 35 then
 		return "shotgun"
@@ -27,7 +30,7 @@ local function get_weapon_class(weapon_id)
 	return "shared"
 end
 
-callbacks.Register("Draw", function()
+callbacks.Register("Draw", jump_scout_fix, function()
 	local local_player = entities.GetLocalPlayer()
 	if not local_player or not local_player:IsAlive() then
 		gui.SetValue("misc.strafe.enable", true)
